1dbb24b68ed25b8487c56e9679cac35a5c2776b3
lisag <lisa.geretsried@gmail.com>
2022-10-06 19:49:48 +0200

    Test-Klasse für den GermanTranslator, die nur die Methode translateNumber testet

---
+++ test/org/hbrs/s1/ws22/uebung1/test/GermanTranslatorTest.java
@@ -0,0 +1,16 @@
+package org.hbrs.s1.ws22.uebung1.test;
+
+import org.hbrs.s1.ws22.uebung1.control.GermanTranslator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GermanTranslatorTest {
+
+    @org.junit.jupiter.api.Test
+    void translateNumber() {
+        GermanTranslator translator = new GermanTranslator();
+        assertEquals("zwei",translator.translateNumber(2));
+        assertEquals("Übersetzung der Zahl -1 nicht möglich(1.0)",translator.translateNumber(-1));
+        assertEquals("Übersetzung der Zahl 12 nicht möglich(1.0)",translator.translateNumber(12));
+    }
+}
