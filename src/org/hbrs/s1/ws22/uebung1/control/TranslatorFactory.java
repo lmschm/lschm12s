006645c19266a25b05c0261e2760db336e8171a2
lisag <lisa.geretsried@gmail.com>
2022-10-06 19:00:21 +0200

    Erstellung der geforderten Fabrikmethode

--- src/org/hbrs/s1/ws22/uebung1/control/TranslatorFactory.java
+++ src/org/hbrs/s1/ws22/uebung1/control/TranslatorFactory.java
@@ -2,4 +2,10 @@ package org.hbrs.s1.ws22.uebung1.control;
 
 public class TranslatorFactory {
 
+    public static Translator erstelleGermanTranslator() {
+        GermanTranslator translator = new GermanTranslator();
+        translator.setDate("06.10.2022");
+        return translator;
+    }
+
 }
