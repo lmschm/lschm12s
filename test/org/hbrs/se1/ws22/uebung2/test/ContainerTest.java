099462b9096542dc327c6c000a410dbb1a340605
lisag <lisa.geretsried@gmail.com>
2022-10-18 09:39:41 +0200

    Erstellung der ContainerTest-Klasse

---
+++ test/org/hbrs/se1/ws22/uebung2/test/ContainerTest.java
@@ -0,0 +1,28 @@
+package org.hbrs.se1.ws22.uebung2.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @Test
+    void containsMember() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void dump() {
+    }
+}
